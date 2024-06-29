a79cc56445a6278d254e172f73b7b8d3ffba9aed
Ray_Gek <dmitry1986antipin@gmail.com>
2024-06-29 18:43:13 +0300

    Hibernate entity

--- src/main/java/PaymentService/entity/Payment.java
+++ src/main/java/PaymentService/entity/Payment.java
@@ -1,14 +1,28 @@
 package PaymentService.entity;
 
+
+
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-@Data
-public class Payment {
-    private Long paymentId;
-    private Double amount;
-    private LocalDateTime paymentDate;
-    private String status;
 
+@Data
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private LocalDateTime paymentDate;
+
+    @Column(nullable = false)
+    private String status;
 }
+
 
